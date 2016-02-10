39c072d35e2c334b68b3c786f015d74109e32fd1
Roshan George <roshan@arjie.com>
2016-02-10 00:24:38 -0800

    Rename exponential delay parameters
        Also only accept longs for the initial delay since it's in milliseconds anyway.

--- src/main/java/com/arjie/groundhog/Retriers.java
+++ src/main/java/com/arjie/groundhog/Retriers.java
@@ -1,9 +1,9 @@
 package com.arjie.groundhog;
 
 import com.arjie.groundhog.impl.ExponentialDelayStrategy;
+import com.arjie.groundhog.impl.FixedTriesFixedDelayRetrier;
 import com.arjie.groundhog.impl.MaxTriesKnownExceptionTryStrategy;
 import com.arjie.groundhog.impl.NumTriesAndExceptionTracker;
-import com.arjie.groundhog.impl.FixedTriesFixedDelayRetrier;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -11,13 +11,10 @@ import java.util.concurrent.Callable;
 
 @SuppressWarnings("unused")
 public class Retriers {
-  public static <V> Retrier<V, NumTriesAndExceptionTracker> fixedTriesFixedDelay(Callable<V> c, int maxTries, long waitMillisBetweenTries) {
-    return new FixedTriesFixedDelayRetrier<>(c, maxTries, waitMillisBetweenTries, Collections.<Class<? extends Exception>>singletonList(Exception.class));
-  }
 
   /**
    * Retry given {@link Callable} up to a fixed maximum number of times, waiting a fixed time in between, and catching
-   * only some {@link Exception}s. An attempt will be made only if the previous attempt failed.
+   * only some {@link Exception}s. A re-attempt will be made only if the previous attempt failed.
    *
    * @param c The {@link Callable} to retry.
    * @param maxTries The maximum number of attempts to make.
@@ -32,24 +29,38 @@ public class Retriers {
   }
 
   /**
+   * Catch all {@link Exception} but otherwise behave like {@link #fixedTriesFixedDelay(Callable, int, long, Collection)}
+   */
+  public static <V> Retrier<V, NumTriesAndExceptionTracker> fixedTriesFixedDelay(Callable<V> c, int maxTries, long waitMillisBetweenTries) {
+    return fixedTriesFixedDelay(c, maxTries, waitMillisBetweenTries, Collections.<Class<? extends Exception>>singletonList(Exception.class));
+  }
+
+  /**
    * Retry given {@link Callable} up to a fixed maximum number of times, exponentially backing off, and catching only
-   * some {@link Exception}s. An attempt will be made only if the previous attempt failed.
+   * some {@link Exception}s. A re-attempt will be made only if the previous attempt failed.
    *
    * @param c The {@link Callable} to retry
    * @param maxTries The maximum number of attempts to make.
-   * @param delayBase The value to multiply the previous delay by to get the current delay
-   * @param delayCoefficient The initial delay
+   * @param delayFactor The value to multiply the previous delay by to get the current delay
+   * @param initialDelayInMillis The initial delay in milliseconds
    * @param exceptionsToRetryOn The exceptions to catch and retry on. Others will be propagated up the chain normally.
    * @param <V> The return value of the {@link Callable}.
    *
    * @return A {@link Callable} that works as described above.
    */
-  public static <V> Retrier<V, NumTriesAndExceptionTracker> fixedTriesExponentialBackoff(Callable<V> c, int maxTries, double delayBase, double delayCoefficient, Collection<Class<? extends Exception>> exceptionsToRetryOn) {
+  public static <V> Retrier<V, NumTriesAndExceptionTracker> fixedTriesExponentialBackoff(Callable<V> c, int maxTries, double delayFactor, long initialDelayInMillis, Collection<Class<? extends Exception>> exceptionsToRetryOn) {
     return new Retrier<>(
         c,
         new MaxTriesKnownExceptionTryStrategy<>(maxTries, exceptionsToRetryOn),
-        new ExponentialDelayStrategy<NumTriesAndExceptionTracker>(delayBase, delayCoefficient),
+        new ExponentialDelayStrategy<NumTriesAndExceptionTracker>(delayFactor, initialDelayInMillis),
         new NumTriesAndExceptionTracker.Factory()
     );
+  }
+
+  /**
+   * Catch all {@link Exception} but otherwise behave like {@link #fixedTriesExponentialBackoff(Callable, int, double, long, Collection)}
+   */
+  public static <V> Retrier<V, NumTriesAndExceptionTracker> fixedTriesExponentialBackoff(Callable<V> c, int maxTries, double delayFactor, long initialDelayInMillis) {
+    return fixedTriesExponentialBackoff(c, maxTries, delayFactor, initialDelayInMillis, Collections.<Class<? extends Exception>>singletonList(Exception.class));
   }
 }
--- src/main/java/com/arjie/groundhog/impl/ExponentialDelayStrategy.java
+++ src/main/java/com/arjie/groundhog/impl/ExponentialDelayStrategy.java
@@ -4,28 +4,27 @@ import com.arjie.groundhog.DelayStrategy;
 import com.arjie.groundhog.TryState;
 
 /**
- * Delay each execution by {@link #base} times the previous delay starting with {@link #coefficient}.
+ * Delay each execution by {@link #factor} times the previous delay, starting with {@link #initial}.
  * Not meant to be exact for large values.
  *
  * @param <S> The type of state, which must have some way to obtain the number of tries.
  */
 public class ExponentialDelayStrategy<S extends NumTriesState & TryState> implements DelayStrategy<S> {
 
-  private final double base;
-  private final double coefficient;
+  private final double factor;
+  private final long initial;
 
-  public ExponentialDelayStrategy(double base) {
-    this(base, 1.0f);
+  public ExponentialDelayStrategy(double factor) {
+    this(factor, 1L);
   }
 
-  public ExponentialDelayStrategy(double base, double coefficient) {
-
-    this.base = base;
-    this.coefficient = coefficient;
+  public ExponentialDelayStrategy(double factor, long initial) {
+    this.factor = factor;
+    this.initial = initial;
   }
 
   @Override
   public long getMillisToDelayRetry(S state) {
-    return Math.round(coefficient * (Math.pow(base, state.getNumTries() - 1)));
+    return Math.round(initial * (Math.pow(factor, state.getNumTries() - 1)));
   }
 }
